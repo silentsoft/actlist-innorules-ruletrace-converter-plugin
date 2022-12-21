83fa94ca84f8330ccbfa13b5f407f5a3b47db76e
silentsoft <hyesung.lee@silentsoft.org>
2022-12-22 01:09:20 +0900

    Add EOT, GS and RS to replacement targets and some replace values are changed

--- src/main/java/RuleTraceUtil.java
+++ src/main/java/RuleTraceUtil.java
@@ -7,8 +7,24 @@ public class RuleTraceUtil {
 		
 		char[] charArray = content.toCharArray();
 		for (int i=0, j=charArray.length; i<j; i++) {
-			if (charArray[i] == PluginConst.REPLACE_TARGET_STX || charArray[i] == PluginConst.REPLACE_TARGET_ETX) {
-				charArray[i] = PluginConst.REPLACE_VALUE_STX_ETX;
+			if (charArray[i] == PluginConst.REPLACE_TARGET_STX) {
+				charArray[i] = PluginConst.REPLACE_VALUE_STX;
+			}
+
+			if (charArray[i] == PluginConst.REPLACE_TARGET_EOT) {
+				charArray[i] = PluginConst.REPLACE_VALUE_EOT;
+			}
+
+			if (charArray[i] == PluginConst.REPLACE_TARGET_GS) {
+				charArray[i] = PluginConst.REPLACE_VALUE_GS;
+			}
+
+			if (charArray[i] == PluginConst.REPLACE_TARGET_RS) {
+				charArray[i] = PluginConst.REPLACE_VALUE_RS;
+			}
+
+			if (charArray[i] == PluginConst.REPLACE_TARGET_ETX) {
+				charArray[i] = PluginConst.REPLACE_VALUE_ETX;
 			}
 		}
 		
--- src/test/java/RuleTraceUtilTest.java
+++ src/test/java/RuleTraceUtilTest.java
@@ -5,18 +5,28 @@ public class RuleTraceUtilTest {
 	
 	@Test
 	public void convertTest() {
-		Assert.assertArrayEquals("".toCharArray(), RuleTraceUtil.convert("<>"));
-		Assert.assertArrayEquals(":".toCharArray(), RuleTraceUtil.convert("<CR><LF>"));
-		Assert.assertArrayEquals(":".toCharArray(), RuleTraceUtil.convert(Character.toString((char) 02)));
-		Assert.assertArrayEquals(":".toCharArray(), RuleTraceUtil.convert(Character.toString((char) 03)));
-		
+		Assert.assertArrayEquals(String.valueOf(PluginConst.REPLACE_VALUE_STX).toCharArray(), RuleTraceUtil.convert(Character.toString(PluginConst.REPLACE_TARGET_STX)));
+		Assert.assertArrayEquals(String.valueOf(PluginConst.REPLACE_VALUE_EOT).toCharArray(), RuleTraceUtil.convert(Character.toString(PluginConst.REPLACE_TARGET_EOT)));
+		Assert.assertArrayEquals(String.valueOf(PluginConst.REPLACE_VALUE_GS).toCharArray(), RuleTraceUtil.convert(Character.toString(PluginConst.REPLACE_TARGET_GS)));
+		Assert.assertArrayEquals(String.valueOf(PluginConst.REPLACE_VALUE_RS).toCharArray(), RuleTraceUtil.convert(Character.toString(PluginConst.REPLACE_TARGET_RS)));
+
+		Assert.assertArrayEquals(String.valueOf(PluginConst.REPLACE_VALUE_ETX).toCharArray(), RuleTraceUtil.convert(Character.toString(PluginConst.REPLACE_TARGET_ETX)));
+		Assert.assertArrayEquals(PluginConst.REPLACE_VALUE_CRLF.toCharArray(), RuleTraceUtil.convert(PluginConst.REPLACE_TARGET_CRLF));
+
+		Assert.assertArrayEquals(PluginConst.REPLACE_VALUE_LTGT.toCharArray(), RuleTraceUtil.convert(PluginConst.REPLACE_TARGET_LTGT));
+		Assert.assertArrayEquals(PluginConst.REPLACE_VALUE_AMPERSAND.toCharArray(), RuleTraceUtil.convert("&"));
+
 		Assert.assertArrayEquals("&amp;".toCharArray(), RuleTraceUtil.convert("&amp;"));
 		Assert.assertArrayEquals("&apos;".toCharArray(), RuleTraceUtil.convert("&apos;"));
 		Assert.assertArrayEquals("&lt;".toCharArray(), RuleTraceUtil.convert("&lt;"));
 		Assert.assertArrayEquals("&gt;".toCharArray(), RuleTraceUtil.convert("&gt;"));
 		Assert.assertArrayEquals("&quot;".toCharArray(), RuleTraceUtil.convert("&quot;"));
-		
-		Assert.assertArrayEquals("&amp;".toCharArray(), RuleTraceUtil.convert("&"));
+
+		Assert.assertArrayEquals("A&lt;&gt;B".toCharArray(), RuleTraceUtil.convert("A<>B"));
+		Assert.assertArrayEquals("A&lt;&gt;B".toCharArray(), RuleTraceUtil.convert("A&lt;&gt;B"));
+		Assert.assertArrayEquals("A&lt;&gt;B A&lt;&gt;B".toCharArray(), RuleTraceUtil.convert("A<>B A&lt;&gt;B"));
+		Assert.assertArrayEquals("A&lt;&gt;B A&lt;&gt;B".toCharArray(), RuleTraceUtil.convert("A&lt;&gt;B A<>B"));
+
 		Assert.assertArrayEquals("A&amp;B".toCharArray(), RuleTraceUtil.convert("A&B"));
 		Assert.assertArrayEquals("A&amp;B".toCharArray(), RuleTraceUtil.convert("A&amp;B"));
 		Assert.assertArrayEquals("A&amp;B A&amp;B".toCharArray(), RuleTraceUtil.convert("A&B A&amp;B"));
